95e93071e4fc17aade0ca3a6d37a46584895b6d4
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-06-02 14:38:53 +0000

    ok

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -16,27 +16,6 @@
                 </li>
             @endif
 
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <li class="pagination-item disabled"><span class="pagination-link">{{ $element }}</span></li>
-                @endif
-
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="pagination-item active"><span class="pagination-link">{{ $page }}</span></li>
-                        @else
-                            <li class="pagination-item">
-                                <a class="pagination-link" href="{{ $url }}">{{ $page }}</a>
-                            </li>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="pagination-item">
