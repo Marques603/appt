90402077f2eee22473ab004a86b1f261b2eff446
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-06-20 15:52:25 -0300

    ok

--- database/migrations/2025_05_06_111820_create_roles_table.php
+++ database/migrations/2025_05_06_111820_create_roles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('module');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -24,11 +25,6 @@ return new class extends Migration
             $table->primary(['user_id', 'role_id']);
         });
 
-        Schema::create('role_document', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
-            $table->primary(['user_id', 'role_id']);
-        });
     }
 
     /**
