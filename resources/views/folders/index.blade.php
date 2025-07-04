409aef0bce8d2e926055b63b001cf23efeed477b
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-07-04 15:57:59 -0300

    ok

--- resources/views/folders/index.blade.php
+++ resources/views/folders/index.blade.php
@@ -158,8 +158,7 @@
                     </div>
                 </div>
             @empty
-                <div class="col-span-full text-center py-12">
-                    <p class="text-slate-500 dark:text-slate-400">Nenhuma pasta encontrada.</p>
+                <div class="col-span-full text-center py-12">                  
                 </div>
             @endforelse
         </div>
